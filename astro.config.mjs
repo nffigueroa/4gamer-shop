1640aeef2f9ed04b26dd7af3b213e97245f6b3c2
nffigueroa <nffigueroag@gmail.com>
2023-01-12 12:17:16 -0500

    latest changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import vue from "@astrojs/vue";
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 
@@ -10,7 +11,10 @@ import tailwind from '@astrojs/tailwind';
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind(), prefetch()],
-  output: 'server'
+  output: 'server',
+  adapter: cloudflare()
 });
