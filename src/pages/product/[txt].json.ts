cd9b4307d5528314aa113bc9428f0e56047c232e
nffigueroa <nffigueroag@gmail.com>
2023-01-12 15:06:41 -0500

    add script for publishing ui

--- src/pages/product/[txt].json.ts
+++ src/pages/product/[txt].json.ts
@@ -1,9 +1,13 @@
 import { APIRoute } from 'astro';
+const secret = import.meta.env.JWRT_SECRET;
 
 export const get: APIRoute = async ({ request, params }) => {
   const { txt } = params;
+  console.log(secret);
 
-  const response = await fetch(`http://127.0.0.1:8787/search/${txt}`);
+  const response = await fetch(
+    `https://4gaming-query.nffigueroag.workers.dev/search/${txt}`
+  );
   const json = await response.json();
   return new Response(JSON.stringify(json));
 };
